6a27ed84c35b6b4ce98d4f2fa15d25d92f96794f
Martin Schwamberger <martin.schwamb@gmail.com>
2015-11-24 23:22:11 +0100

    calc most of workload values

--- src/main/java/veloziped/ws1516/workload/WorkloadPlanning.java
+++ src/main/java/veloziped/ws1516/workload/WorkloadPlanning.java
@@ -8,7 +8,6 @@ package veloziped.ws1516.workload;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import veloziped.ws1516.main.SharedInstance;
 import veloziped.ws1516.workplace.ExtendedWorkplace;
 import veloziped.ws1516.workplace.ProcessTime;
 
@@ -17,34 +16,39 @@ import veloziped.ws1516.workplace.ProcessTime;
  * @author Martin
  */
 public class WorkloadPlanning {
-
-    private final long limitShift1;
-    private final long limitShift2;
-    private final long limitShift3;
-    private final long limitOvertime;
-    private Map<String, WorkloadResults> workloadResults;
-
-    public WorkloadPlanning() {
-        this.limitShift3 = 7200;
-        this.limitShift2 = 4800;
-        this.limitShift1 = 2400;
-        this.limitOvertime = 1200;
-
-        this.reloadWorkloadResults();
+    public static final long LIMITPERSHIFT = 2400;
+    public static final long LIMITOVERTIME = 1200;
+    private Map<String, WorkloadResult> workloadResults;
+    private WorkloadPlanning() {}
+    
+    public static WorkloadPlanning getInstance() {
+        return SharedInstanceHolder.INSTANCE;
     }
+    
+    private static class SharedInstanceHolder {
 
-    private void reloadWorkloadResults() {
+        private static final WorkloadPlanning INSTANCE = new WorkloadPlanning();
+    }
+    
+    private Map<String, WorkloadResult> calculateWorkload(Map<String, ExtendedWorkplace> workplaces) {
         workloadResults = new HashMap<>();
-        
-        Map<String, ExtendedWorkplace> workplaces = SharedInstance.getInstance().getWorkplaces();
 
         for (Map.Entry<String, ExtendedWorkplace> entry : workplaces.entrySet()) {
             ExtendedWorkplace place = entry.getValue();
-            
+
             List<ProcessTime> pT = place.getProcessTimes();
-            
-            WorkloadResults results = new WorkloadResults(place.getId(), pT);
-            workloadResults.put(entry.getKey(), results);
+
+            if (pT.size() > 0) {
+                WorkloadResult results = new WorkloadResult(place);
+                results.reCalculateResults();
+                workloadResults.put(entry.getKey(), results);
+            }
         }
+        
+        return workloadResults;
+    }
+    
+    public Map<String, WorkloadResult> getWorkloadResults() {
+        return workloadResults;
     }
 }
---
+++ src/main/java/veloziped/ws1516/workload/WorkloadResult.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package veloziped.ws1516.workload;
+
+import static java.lang.Math.ceil;
+import static java.lang.Math.round;
+import java.text.DecimalFormat;
+import veloziped.ws1516.main.SharedInstance;
+import veloziped.ws1516.workplace.*;
+
+/**
+ *
+ * @author Martin
+ */
+public class WorkloadResult {
+
+    //TODO: Kap. Bed. (Rückstand Vorperiode)
+    private final ExtendedWorkplace workplace;
+    private long capacityNeeded;
+    private long setupTime;
+    private long lastSetupCycles;
+    private double setupFactor;
+    private long additionalSetupTime;
+    private final long backlogCapacityLastPeriod;
+    private final long backlogSetupTimeLastPeriod;
+    private long totalCapacityNeeded;
+    private long numberOfShifts;
+    private long overTimePeriod;
+    private long overTimeDay;
+    private long freeCapacity;
+    private double workloadPercentage;
+
+    public WorkloadResult(ExtendedWorkplace workplace) {
+        this.workplace = workplace;
+        this.lastSetupCycles = workplace.getSetupevents();
+
+        //TODO get from xml
+        this.backlogCapacityLastPeriod = 0;
+        this.backlogSetupTimeLastPeriod = 0;
+
+        this.capacityNeeded = 0;
+        this.setupTime = 0;
+        this.setupFactor = 0;
+        this.additionalSetupTime = 0;
+        this.totalCapacityNeeded = 0;
+        this.numberOfShifts = 0;
+        this.overTimePeriod = 0;
+        this.overTimeDay = 0;
+        this.freeCapacity = 0;
+        this.workloadPercentage = 0;
+    }
+
+    public long getBacklogSetupTimeLastPeriod() {
+        return backlogSetupTimeLastPeriod;
+    }
+
+    public double getWorkloadPercentage() {
+        return workloadPercentage;
+    }
+
+    public long getFreeCapacity() {
+        return freeCapacity;
+    }
+
+    public long getOverTimeDay() {
+        return overTimeDay;
+    }
+
+    public long getOverTimePeriod() {
+        return overTimePeriod;
+    }
+
+    public long getNumberOfShifts() {
+        return numberOfShifts;
+    }
+
+    public long getTotalCapacityNeeded() {
+        return totalCapacityNeeded;
+    }
+
+    public long getBacklogCapacityLastPeriod() {
+        return backlogCapacityLastPeriod;
+    }
+
+    public long getAdditionalSetupTime() {
+        return additionalSetupTime;
+    }
+
+    public double getSetupFactor() {
+        return setupFactor;
+    }
+
+    public long getLastSetupCycles() {
+        return lastSetupCycles;
+    }
+
+    public void setLastSetupCycles(long lastSetupCycles) {
+        this.lastSetupCycles = lastSetupCycles;
+    }
+
+    public long getSetupTime() {
+        return setupTime;
+    }
+
+    public long getCapacityNeeded() {
+        return capacityNeeded;
+    }
+
+    public ExtendedWorkplace getWorkplaceId() {
+        return workplace;
+    }
+
+    public void reCalculateResults() {
+        this.calcCapacityNeeded();
+        this.calcSetupTime();
+        this.calcAdditionalSetupTime();
+        this.calcTotalCapacityNeeded();
+        this.calcNumberOfShifts();
+        this.calcOverTime();
+        this.calcFreeCapacity();
+        this.calcWorkloadPercentage();
+    }
+
+    private void calcCapacityNeeded() {
+        for (ProcessTime process : this.workplace.getProcessTimes()) {
+            Long orderQuantity = SharedInstance.getInstance().getOrderQuantityForArticleId(process.getArticleId());
+
+            if (orderQuantity != null) {
+                this.capacityNeeded += orderQuantity * process.getRunTime();
+            }
+        }
+    }
+
+    private void calcSetupTime() {
+        for (ProcessTime process : this.workplace.getProcessTimes()) {
+            Long orderQuantity = SharedInstance.getInstance().getOrderQuantityForArticleId(process.getArticleId());
+
+            if (orderQuantity != null && orderQuantity > 0) {
+                this.setupTime += process.getSetupTime();
+            }
+        }
+    }
+
+    private void calcAdditionalSetupTime() {
+        int size = this.workplace.getProcessTimes().size();
+
+        if (size > 0) {
+            this.setupFactor = this.lastSetupCycles / this.workplace.getProcessTimes().size();
+        }
+
+        this.additionalSetupTime = (long) ceil(this.setupTime * this.setupFactor);
+    }
+
+    private void calcTotalCapacityNeeded() {
+        this.totalCapacityNeeded = this.capacityNeeded + this.setupTime
+                + this.additionalSetupTime + this.backlogCapacityLastPeriod
+                + this.backlogSetupTimeLastPeriod;
+    }
+
+    private void calcNumberOfShifts() {
+        //TODO 
+    }
+
+    private void calcOverTime() {
+        if (this.totalCapacityNeeded > (this.numberOfShifts * WorkloadPlanning.LIMITPERSHIFT)) {
+            this.overTimePeriod = this.totalCapacityNeeded - (this.numberOfShifts * WorkloadPlanning.LIMITPERSHIFT);
+        } else {
+            this.overTimePeriod = 0;
+        }
+
+        this.overTimeDay = round(this.overTimePeriod / 5);
+    }
+
+    private void calcFreeCapacity() {
+        if (this.totalCapacityNeeded < (this.numberOfShifts * WorkloadPlanning.LIMITPERSHIFT)) {
+            this.freeCapacity = (this.numberOfShifts * WorkloadPlanning.LIMITPERSHIFT) - this.totalCapacityNeeded;
+        } else {
+            this.freeCapacity = 0;
+        }
+    }
+
+    private void calcWorkloadPercentage() {
+        double perct = this.totalCapacityNeeded / (this.numberOfShifts * WorkloadPlanning.LIMITPERSHIFT);
+        DecimalFormat df = new DecimalFormat("#.##");
+        this.workloadPercentage = Double.valueOf(df.format(perct));
+    }
+}
